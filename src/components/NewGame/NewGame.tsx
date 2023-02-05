137cb2f1d5dca7ebcebd15ec33630212d22fffe6
Trentin Gillis <trentin.gillis@gmail.com>
2023-02-05 11:55:18 -0700

    Add initial layout for NewGame component

--- src/components/NewGame/NewGame.tsx
+++ src/components/NewGame/NewGame.tsx
@@ -1,3 +1,12 @@
+import styles from './NewGame.module.css';
+
 export function NewGame() {
-  return <h1>NewGame</h1>;
+  return (
+    <div className={styles.layout}>
+      <h1>stuff</h1>
+      {/* TODO: Logo */}
+      {/* TODO: PlayerSelect */}
+      {/* TODO: NewGameButtons */}
+    </div>
+  );
 }
