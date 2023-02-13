d994b5bad982563412fb5ed4922c59681c58f7d2
Trentin Gillis <trentin.gillis@gmail.com>
2023-02-13 14:44:57 -0700

    Migrate GameBoardHeader to styled-components

--- src/components/GameBoardHeader/GameBoardHeader.tsx
+++ src/components/GameBoardHeader/GameBoardHeader.tsx
@@ -1,24 +1,57 @@
-import styles from './GameBoardHeader.module.css';
+import styled from 'styled-components';
 
-import resetIcon from '@/assets/icon-restart.svg';
+import restartIcon from '@/assets/icon-restart.svg';
 import { Button, Logo, TurnDisplay } from '@/components';
 import { useGameEngine } from '@/lib/hooks/useGameEngine';
+
+const Layout = styled.article`
+  grid-area: header;
+  height: 40px;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-template-areas: 'logo turn reset';
+
+  @media only screen and (min-width: 768px) {
+    height: 52px;
+  }
+`;
+
+const LogoWrapper = styled.span`
+  grid-area: logo;
+  display: flex;
+  align-items: center;
+`;
+
+const ButtonWrapper = styled.span`
+  grid-area: reset;
+  width: 100%;
+  display: flex;
+  justify-content: right;
+
+  @media only screen and (min-width: 768px) {
+    button {
+      padding: 16px;
+      height: 52px;
+      width: 52px;
+    }
+  }
+`;
 
 export function GameBoardHeader() {
   const gameEngine = useGameEngine();
 
   return (
-    <article className={styles.layout}>
-      <span className={styles['logo-wrapper']}>
+    <Layout>
+      <LogoWrapper>
         <Logo />
-      </span>
+      </LogoWrapper>
       <TurnDisplay />
-      <span className={styles['reset-btn-wrapper']}>
+      <ButtonWrapper>
         {/* TODO: Update button to pop modal to reset the game */}
         <Button variant="secondary" color="silver" onClick={() => gameEngine?.clearBoard()}>
-          <img src={resetIcon} alt="" />
+          <img src={restartIcon} alt="restart button icon" />
         </Button>
-      </span>
-    </article>
+      </ButtonWrapper>
+    </Layout>
   );
 }
