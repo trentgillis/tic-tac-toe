199248c6744adf617309ed43388b985c570665d4
Trentin Gillis <trentin.gillis@gmail.com>
2023-02-13 14:58:25 -0700

    Migrate GameBoardCell to styled-components

--- src/components/GameBoardCell/GameBoardCell.tsx
+++ src/components/GameBoardCell/GameBoardCell.tsx
@@ -6,11 +6,53 @@ import oMarkImg from '@/assets/icon-o.svg';
 import { useGameEngine } from '@/lib/hooks/useGameEngine';
 import { useEffect, useState } from 'react';
 import { ValidTokens } from '@/lib/types/ValidTokens';
+import styled from 'styled-components';
 
 type GameBoardCellProps = {
   row: number;
   col: number;
 };
+
+const Layout = styled.div`
+  width: auto;
+  height: auto;
+  max-width: 100%;
+  max-height: 100%;
+  background-color: var(--color-semi-dark-navy);
+  box-shadow: inset 0px -8px 0px #10212a;
+  border-radius: 15px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  aspect-ratio: 1 / 1;
+
+  &:hover:not(.populated) {
+    cursor: pointer;
+  }
+
+  @media only screen and (min-width: 768px) {
+    &.current-player-x:hover:not(.populated) > * {
+      background: url('/src/assets/icon-x-outline.svg') no-repeat center;
+    }
+
+    &.current-player-o:hover:not(.populated) > * {
+      background: url('/src/assets/icon-o-outline.svg') no-repeat center;
+    }
+
+    &.populated {
+      cursor: default;
+    }
+  }
+`;
+
+const IconWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-size: 64px 64px;
+`;
 
 export function GameBoardCell({ row, col }: GameBoardCellProps) {
   const gameEngine = useGameEngine();
@@ -28,15 +70,14 @@ export function GameBoardCell({ row, col }: GameBoardCellProps) {
   };
 
   return (
-    <div
-      className={`${styles['game-board-cell']} 
-        ${styles[`current-player-${gameEngine?.currentPlayer}`]}
-        ${mark ? styles['populated'] : ''}`}
+    <Layout
+      className={`
+        ${`current-player-${gameEngine?.currentPlayer}`}
+        ${mark ? 'populated' : ''}
+      `}
       onClick={() => handleCellClick()}
     >
-      <div className={styles['mark-icon']}>
-        {mark && <img src={mark === 'x' ? xMarkImg : oMarkImg} />}
-      </div>
-    </div>
+      <IconWrapper>{mark && <img src={mark === 'x' ? xMarkImg : oMarkImg} />}</IconWrapper>
+    </Layout>
   );
 }
