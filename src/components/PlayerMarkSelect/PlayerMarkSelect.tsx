9c639105c8ce491bbc5b03d7744ebc1be61b8f02
Trentin Gillis <trentin.gillis@gmail.com>
2023-02-13 14:24:41 -0700

    Migrate PlayerMarkSelect to styled-components

--- src/components/PlayerMarkSelect/PlayerMarkSelect.tsx
+++ src/components/PlayerMarkSelect/PlayerMarkSelect.tsx
@@ -1,4 +1,4 @@
-import styles from './PlayerSelect.module.css';
+import styled from 'styled-components';
 
 import iconXDark from '@/assets/icon-x-dark-navy.svg';
 import iconXLight from '@/assets/icon-x-silver.svg';
@@ -12,11 +12,34 @@ type PlayerMarkSelectProps = {
   setPlayerOneToken: Function;
 };
 
+const Layout = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 24px;
+  gap: 24px;
+  background-color: var(--color-semi-dark-navy);
+  border-radius: 15px;
+  width: 100%;
+  box-shadow: inset 0px -8px 0px #10212a;
+`;
+
+const ButtonWrapper = styled.div`
+  padding: 8px;
+  height: 72px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  border-radius: 10px;
+  background-color: var(--color-dark-navy);
+`;
+
 export function PlayerMarkSelect({ playerOneToken, setPlayerOneToken }: PlayerMarkSelectProps) {
   return (
-    <section className={styles.layout}>
+    <Layout>
       <H4 color="silver">pick player 1&apos;s mark</H4>
-      <div className={styles['mark-buttons-wrapper']}>
+      <ButtonWrapper>
         <MarkButton
           imgUrlDark={iconXDark}
           imgUrlLight={iconXLight}
@@ -31,8 +54,8 @@ export function PlayerMarkSelect({ playerOneToken, setPlayerOneToken }: PlayerMa
           playerOneToken={playerOneToken}
           setPlayerOneToken={setPlayerOneToken}
         />
-      </div>
+      </ButtonWrapper>
       <BodyText color="silver">remember&#58; x goes first</BodyText>
-    </section>
+    </Layout>
   );
 }
